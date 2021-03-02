d2c350090f0a252cf0d4b201c1d9bfdec1c6a829
unknown <nitzanmaizel@gmail.com>
2021-03-02 07:02:11 +0200

    Index.js - Create Routh with auth - onAuthStateChange

--- src/index.js
+++ src/index.js
@@ -1,23 +1,48 @@
-import React from "react";
+import React, { useEffect } from "react";
 import ReactDOM from "react-dom";
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  withRouter,
+} from "react-router-dom";
+import { firebase } from "./firebase";
 import App from "./components/App.js";
 import Login from "./components/Auth/Login.jsx";
 import Register from "./components/Auth/Register.jsx";
 import reportWebVitals from "./reportWebVitals";
 import "semantic-ui-css/semantic.min.css";
 
-const Root = () => (
-  <Router>
+const Root = (props) => {
+  const isUserLogin = async () => {
+    await firebase.auth().onAuthStateChanged((user) => {
+      if (user) {
+        props.history.push("/");
+      }
+    });
+  };
+
+  useEffect(() => {
+    isUserLogin();
+  }, []);
+
+  return (
     <Switch>
       <Route exact path="/" component={App} />
       <Route path="/login" component={Login} />
       <Route path="/register" component={Register} />
     </Switch>
-  </Router>
-);
+  );
+};
 
-ReactDOM.render(<Root />, document.getElementById("root"));
+const RootWithAuth = withRouter(Root);
+
+ReactDOM.render(
+  <Router>
+    <RootWithAuth />
+  </Router>,
+  document.getElementById("root")
+);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
